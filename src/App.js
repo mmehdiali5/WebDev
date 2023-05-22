6b1a1263c9d10a3d288ac09802ef47604abb53f9
mmehdiali5 <mmehdiali5@gmail.com>
2023-05-22 19:47:31 -0400

    Hello World React

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import './App.css';
 
 function App() {
   return (
+      <h1>Hello World!</h1>
+  );
+}
+export default App;
+
+/*function App() {
+  return (
     <div className="App">
     </div>
   );
 }
 
-export default App;
+export default App;*/
