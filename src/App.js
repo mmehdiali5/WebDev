9de6e981f2dcac0f80db3147a26981f169afc7f8
mmehdiali5 <mmehdiali5@gmail.com>
2023-05-23 01:16:38 -0400

    BrowseRouterAdded

--- src/App.js
+++ src/App.js
@@ -4,14 +4,22 @@ import Labs from "./labs";
 import Tuiter from "./tuiter";
 import HelloWorld from "./labs/a3/hello-world";
 import {BrowserRouter} from "react-router-dom";
+import {Routes, Route} from "react-router"
 
 function App() {
     return (
         <BrowserRouter>
             <div className="container">
-                <HelloWorld/>
-                <Labs/>
-                <Tuiter/>
+                <Routes>
+                    <Route path="/labs/*"
+                           element={<Labs/>}/>
+                    <Route path="/hello"
+                           element={<HelloWorld/>}/>
+                    <Route path="/tuiter/*"
+                           element={<Tuiter/>}/>
+                </Routes>
+
+
             </div>
 
         </BrowserRouter>
