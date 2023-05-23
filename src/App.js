23a84962468c8ecda355beb2e9318c85866c901d
mmehdiali5 <mmehdiali5@gmail.com>
2023-05-23 00:53:32 -0400

    BrowseRouterAdded

--- src/App.js
+++ src/App.js
@@ -2,17 +2,24 @@
 import './App.css';
 import Labs from "./labs";
 import Tuiter from "./tuiter";
+import HelloWorld from "./labs/a3/hello-world";
+import {BrowserRouter} from "react-router-dom";
 
 function App() {
-  return (
-      <div className="container">
-        <h1>Hello World!</h1>
-          <Labs/>
-          <Tuiter/>
-      </div>
+    return (
+        <BrowserRouter>
+            <div className="container">
+                <HelloWorld/>
+                <Labs/>
+                <Tuiter/>
+            </div>
 
-  );
+        </BrowserRouter>
+
+
+    );
 }
+
 export default App;
 
 /*function App() {
