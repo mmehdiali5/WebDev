eea707d93d164f020a400e0da75c407baf51d5c3
mmehdiali5 <mmehdiali5@gmail.com>
2023-05-23 01:44:01 -0400

    A3 till Section 2.3 completed

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ function App() {
                 <Routes>
                     <Route path="/"
                     element={<Navigate to="/labs/"/>}/>
-
                     <Route path="/labs/*"
                            element={<Labs/>}/>
                     <Route path="/hello"
--- src/labs/index.js
+++ src/labs/index.js
@@ -1,8 +1,10 @@
 import Assignment3 from "./a3";
+import Nav from "../nav";
 
 function Labs() {
     return(
         <div>
+            <Nav/>
             {/*<h1>Assignment 3</h1>-->*/}
             <Assignment3/>
         </div>
